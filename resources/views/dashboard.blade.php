e95cb03541c6382164cc7ca999addec2651062e7
aaaa777 <aaaa777@users.noreply.github.com>
2023-08-17 11:07:23 +0900

    チャレンジページなどのファイルを作成

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>画像投稿サイト</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+<!-- ヘッダー -->
+<header class="bg-dark text-white text-center py-4">
+    <h1>画像投稿サイト</h1>
+</header>
+
+<!-- メインコンテンツ -->
+<div class="container mt-4">
+    <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4">
+
+        <!-- カード 1 -->
+        <div class="col">
+            <div class="card card-lg">
+                <img src="http://localhost/img/draft.png" class="card-img-top" alt="画像の説明">
+                <div class="card-body">
+                    <h5 class="card-title">画像タイトル</h5>
+                </div>
+            </div>
+        </div>
+
+        <!-- カード 2 -->
+        <div class="col">
+            <div class="card card-lg">
+                <img src="http://localhost/img/draft.png" class="card-img-top" alt="画像の説明">
+                <div class="card-body">
+                    <h5 class="card-title">画像タイトル</h5>
+                </div>
+            </div>
+        </div>
+
+        <!-- カード 2 -->
+        <div class="col">
+            <div class="card card-lg">
+                <img src="http://localhost/img/draft.png" class="card-img-top" alt="画像の説明">
+                <div class="card-body">
+                    <h5 class="card-title">画像タイトル</h5>
+                </div>
+            </div>
+        </div>
+        <!-- カード 2 -->
+        <div class="col">
+            <div class="card card-lg">
+                <img src="http://localhost/img/draft.png" class="card-img-top" alt="画像の説明">
+                <div class="card-body">
+                    <h5 class="card-title">画像タイトル</h5>
+                </div>
+            </div>
+        </div>
+        <!-- カード 2 -->
+        <div class="col">
+            <div class="card card-lg">
+                <img src="http://localhost/img/draft.png" class="card-img-top" alt="画像の説明">
+                <div class="card-body">
+                    <h5 class="card-title">画像タイトル</h5>
+                </div>
+            </div>
+        </div>
+        <!-- カード 2 -->
+        <div class="col">
+            <div class="card card-lg">
+                <img src="http://localhost/img/draft.png" class="card-img-top" alt="画像の説明">
+                <div class="card-body">
+                    <h5 class="card-title">画像タイトル</h5>
+                </div>
+            </div>
+        </div>
+        <!-- カード 2 -->
+        <div class="col">
+            <div class="card card-lg">
+                <img src="http://localhost/img/draft.png" class="card-img-top" alt="画像の説明">
+                <div class="card-body">
+                    <h5 class="card-title">画像タイトル</h5>
+                </div>
+            </div>
+        </div>
+        <!-- カード 2 -->
+        <div class="col">
+            <div class="card card-lg">
+                <img src="http://localhost/img/draft.png" class="card-img-top" alt="画像の説明">
+                <div class="card-body">
+                    <h5 class="card-title">画像タイトル</h5>
+                </div>
+            </div>
+        </div>
+        <!-- カード 2 -->
+        <div class="col">
+            <div class="card card-lg">
+                <img src="http://localhost/img/draft.png" class="card-img-top" alt="画像の説明">
+                <div class="card-body">
+                    <h5 class="card-title">画像タイトル</h5>
+                </div>
+            </div>
+        </div>
+        <!-- カード 2 -->
+        <div class="col">
+            <div class="card card-lg">
+                <img src="http://localhost/img/draft.png" class="card-img-top" alt="画像の説明">
+                <div class="card-body">
+                    <h5 class="card-title">画像タイトル</h5>
+                </div>
+            </div>
+        </div>
+        <!-- カード 2 -->
+        <div class="col">
+            <div class="card card-lg">
+                <img src="http://localhost/img/draft.png" class="card-img-top" alt="画像の説明">
+                <div class="card-body">
+                    <h5 class="card-title">画像タイトル</h5>
+                </div>
+            </div>
+        </div>
+        <!-- カード 2 -->
+        <div class="col">
+            <div class="card card-lg">
+                <img src="http://localhost/img/draft.png" class="card-img-top" alt="画像の説明">
+                <div class="card-body">
+                    <h5 class="card-title">画像タイトル</h5>
+                </div>
+            </div>
+        </div>
+        <!-- 他のカードも同様に繰り返し -->
+
+    </div>
+</div>
+
+<!-- フッター -->
+<footer class="bg-dark text-white text-center py-3">
+    &copy; 2023 画像投稿サイト. All rights reserved.
+</footer>
+
+</body>
+</html>
